17619cff925c1425aa69f74a9a4a9eff148953c3
Po Chun, Lu <oceanus11034@gmail.com>
2017-08-04 16:05:04 +0800

    [Fix] query without start & end

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,23 @@ const sequelize = require('../models/mcc.js')
 
 /* GET home page. */
 router.get('/mcc', function (req, res, next) {
-  sequelize.query('SELECT * FROM mccs WHERE date(created_at) >= ? AND date(created_at) <= ?',
-    {
-      replacements: [req.query.start, req.query.end], type: sequelize.QueryTypes.SELECT 
-    }).then((mccs) => {
-      res.json({'mccs': mccs})
-    })
+  let query
+  let queryValue = {type: sequelize.QueryTypes.SELECT}
+  if (req.query.start & req.query.end) {
+    query = 'SELECT * FROM mccs WHERE date(created_at) >= ? AND date(created_at) <= ?'
+    queryValue.replacements = [req.query.start, req.query.end]
+  } else if (req.query.start) {
+    query = 'SELECT * FROM mccs WHERE date(created_at) >= ?'
+    queryValue.replacements = [req.query.start]
+  } else if (req.query.end) {
+    query = 'SELECT * FROM mccs WHERE date(created_at) <= ?'
+    queryValue.replacements = [req.query.end]
+  } else {
+    query = 'SELECT * FROM mccs'
+  }
+  sequelize.query(query, queryValue).then((mccs) => {
+    res.json({'mccs': mccs})
+  })
 })
 
 module.exports = router
