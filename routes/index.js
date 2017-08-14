5cefc540ffb490185f9abfa9146af6c81a1b5f76
Po Chun, Lu <oceanus11034@gmail.com>
2017-08-14 16:01:30 +0800

    [Add] lamps API

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,24 @@ router.get('/mcc', function (req, res, next) {
   })
 })
 
+router.get('/lamps', function (req, res, next) {
+  let query
+  let queryValue = {type: sequelize.QueryTypes.SELECT}
+  if (req.query.start & req.query.end) {
+    query = 'SELECT * FROM lamps WHERE date(created_at) >= ? AND date(created_at) <= ?'
+    queryValue.replacements = [req.query.start, req.query.end]
+  } else if (req.query.start) {
+    query = 'SELECT * FROM lamps WHERE date(created_at) >= ?'
+    queryValue.replacements = [req.query.start]
+  } else if (req.query.end) {
+    query = 'SELECT * FROM lamps WHERE date(created_at) <= ?'
+    queryValue.replacements = [req.query.end]
+  } else {
+    query = 'SELECT * FROM lamps'
+  }
+  sequelize.query(query, queryValue).then((lamps) => {
+    res.json({'lamps': lamps})
+  })
+})
+
 module.exports = router
