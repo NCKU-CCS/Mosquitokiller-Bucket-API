0773a2ac13799102229d4afe6ccc811a235b17a7
Po Chun, Lu <oceanus11034@gmail.com>
2017-09-25 14:08:56 +0800

    [Add] CORS support

--- app.js
+++ app.js
@@ -5,10 +5,24 @@ const logger = require('morgan')                  // log
 const cookieParser = require('cookie-parser')     // use in session token
 const bodyParser = require('body-parser')         // Parse Request
 const compression = require('compression')        // Gzip
+const cors = require('cors')
 
-const index = require('./routes/index')
+// ###################################
+// EXPRESS SETUP
+// ###################################
 
 const app = express()
+
+const CONFIG = (process.env.NODE_ENV === 'production')
+? require('../config/lampProdConfig.js')
+: require('../config/lampDevConfig.js')
+
+// set up cors config
+const corsOptions = {
+  origin: CONFIG.cors.url,
+  optionsSucccessCode: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204 
+  credentials: true
+}
 
 global.ROOT_PATH = path.resolve(__dirname)
 
@@ -16,6 +30,9 @@ global.ROOT_PATH = path.resolve(__dirname)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
+// ###################################
+// EXPRESS MIDDLEWARE
+// ###################################
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
@@ -23,9 +40,14 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 // app.use(express.static(path.join(__dirname, 'public')))
-// enable Gzip
-app.use(compression())
 
+app.use(compression())               // enable Gzip
+app.use('/', cors(corsOptions))      // support cors request
+
+// ###################################
+// Route SETUP
+// ###################################
+// const index = require('./routes/index')
 // app.use('/api', index)
 
 const apis = require('./src/api.js')
