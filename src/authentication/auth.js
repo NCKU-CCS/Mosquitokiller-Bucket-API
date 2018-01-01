c961b987c2189246befe13203f1c4b6d6ef0ee6c
Po Chun, Lu <oceanus11034@gmail.com>
2018-01-01 17:39:56 +0800

    [Add] Auth Checked Method that return 401

--- src/authentication/auth.js
+++ src/authentication/auth.js
@@ -6,6 +6,13 @@ module.exports = {
       res.redirect('/login')
     }
   },
+  isAuth: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    } else {
+      res.status(401).json({error: 'not Auth'})
+    }
+  },
   isAdmin: (req, res, next) => {
     if (req.user.role_id === 'ADMIN') {
       return next()
--- src/lampAPI/api.js
+++ src/lampAPI/api.js
@@ -18,9 +18,9 @@ const router = express.Router()
 //
 router.get('/places', Places.getAll)
 router.get('/places/:id', Places.ValidateIdParams, Places.getById)
-router.post('/places', Auth.isLoggedIn, Places.ValidateCreateKeys, Places.create)
-router.put('/places/:id', Auth.isLoggedIn, Places.update)
-router.delete('/places/:id', Auth.isLoggedIn, Places.delete)
+router.post('/places', Auth.isAuth, Places.ValidateCreateKeys, Places.create)
+router.put('/places/:id', Auth.isAuth, Places.update)
+router.delete('/places/:id', Auth.isAuth, Places.delete)
 
 //
 // Lamps API
@@ -28,53 +28,53 @@ router.delete('/places/:id', Auth.isLoggedIn, Places.delete)
 
 router.get('/lamps', Lamps.getAll)
 router.get('/lamps/:id', Lamps.ValidateIdParams, Lamps.getById)
-router.post('/lamps', Auth.isLoggedIn, Lamps.ValidateCreateKeys, Lamps.create)
-router.put('/lamps/:id', Auth.isLoggedIn, Lamps.update)
-router.delete('/lamps/:id', Auth.isLoggedIn, Lamps.delete)
+router.post('/lamps', Auth.isAuth, Lamps.ValidateCreateKeys, Lamps.create)
+router.put('/lamps/:id', Auth.isAuth, Lamps.update)
+router.delete('/lamps/:id', Auth.isAuth, Lamps.delete)
 
 //
 // Counts API
 //
 router.get('/counts', Counts.getAll)
 router.get('/counts/:id', Counts.ValidateIdParams, Counts.getById)
-router.post('/counts', Auth.isLoggedIn, Counts.ValidateCreateKeys, Counts.create)
-router.put('/counts/:id', Auth.isLoggedIn, Counts.update)
-router.delete('/counts/:id', Auth.isLoggedIn, Counts.delete)
+router.post('/counts', Auth.isAuth, Counts.ValidateCreateKeys, Counts.create)
+router.put('/counts/:id', Auth.isAuth, Counts.update)
+router.delete('/counts/:id', Auth.isAuth, Counts.delete)
 
 //
 // States API
 //
 router.get('/states', States.getAll)
 router.get('/states/:id', States.ValidateIdParams, States.getById)
-router.post('/states', Auth.isLoggedIn, States.ValidateCreateKeys, States.create)
-router.put('/states/:id', Auth.isLoggedIn, States.update)
-router.delete('/states/:id', Auth.isLoggedIn, States.delete)
+router.post('/states', Auth.isAuth, States.ValidateCreateKeys, States.create)
+router.put('/states/:id', Auth.isAuth, States.update)
+router.delete('/states/:id', Auth.isAuth, States.delete)
 
 //
 // Mcc API
 //
 router.get('/mcc', Mcc.getAll)
 router.get('/mcc/:id', Mcc.ValidateIdParams, Mcc.getById)
-router.post('/mcc', Auth.isLoggedIn, Mcc.ValidateCreateKeys, Mcc.create)
-router.put('/mcc/:id', Auth.isLoggedIn, Mcc.update)
-router.delete('/mcc/:id', Auth.isLoggedIn, Mcc.delete)
+router.post('/mcc', Auth.isAuth, Mcc.ValidateCreateKeys, Mcc.create)
+router.put('/mcc/:id', Auth.isAuth, Mcc.update)
+router.delete('/mcc/:id', Auth.isAuth, Mcc.delete)
 
 //
 // Rules API
 //
 router.get('/rules', Rules.getAll)
 router.get('/rules/:id', Rules.ValidateIdParams, Rules.getById)
-router.post('/rules', Auth.isLoggedIn, Rules.ValidateCreateKeys, Rules.create)
-router.put('/rules/:id', Auth.isLoggedIn, Rules.update)
-router.delete('/rules/:id', Auth.isLoggedIn, Rules.delete)
+router.post('/rules', Auth.isAuth, Rules.ValidateCreateKeys, Rules.create)
+router.put('/rules/:id', Auth.isAuth, Rules.update)
+router.delete('/rules/:id', Auth.isAuth, Rules.delete)
 
 //
 // Comments API
 //
-router.get('/comments', Auth.isLoggedIn, Comments.getAll)
-router.get('/comments/:id', Auth.isLoggedIn, Comments.ValidateIdParams, Comments.getById)
+router.get('/comments', Auth.isAuth, Comments.getAll)
+router.get('/comments/:id', Auth.isAuth, Comments.ValidateIdParams, Comments.getById)
 router.post('/comments', Comments.ValidateCreateKeys, Comments.create)
-router.put('/comments/:id', Auth.isLoggedIn, Comments.update)
-router.delete('/comments/:id', Auth.isLoggedIn, Comments.delete)
+router.put('/comments/:id', Auth.isAuth, Comments.update)
+router.delete('/comments/:id', Auth.isAuth, Comments.delete)
 
 module.exports = router
